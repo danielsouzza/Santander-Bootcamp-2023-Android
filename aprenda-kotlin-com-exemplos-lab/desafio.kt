ec8f16ee3add827926dd5f16118aab0a85c42db7
danielsouzza <daniel.lima.souzza@gmail.com>
2023-08-17 23:20:32 -0300

    Desafio 1

--- aprenda-kotlin-com-exemplos-lab/desafio.kt
+++ aprenda-kotlin-com-exemplos-lab/desafio.kt
@@ -4,19 +4,26 @@ enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 data class Usuario(val name:String)
 
-data class ConteudoEducacional(val nome: String, val duracao: Int = 60, nivel: Nivel)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val  nivel: Nivel)
 
 data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        inscritos += usuario
+        inscritos.add(usuario)
     }
 
     fun showConteudoEducacional(){
-        inscritos.forEach{
-            
+        conteudos.forEach(){
+            println("Nome: ${it.nome} \t| duração: ${it.duracao} | Nivel: ${it.nivel}")
+        }
+    }
+    
+    fun showInscritos(){
+        println("Número de inscritos: ${inscritos.size}")
+        inscritos.forEach(){
+            println("Nome: ${it.name}")
         }
     }
 }
@@ -30,6 +37,11 @@ fun main() {
                 ConteudoEducacional("Condições",30, Nivel.BASICO)
             )
         )
+    bootcampKotlin.matricular(
+        Usuario("Daniel")
+    )
+    bootcampKotlin.showConteudoEducacional()
+    bootcampKotlin.showInscritos()
 
     
 }
